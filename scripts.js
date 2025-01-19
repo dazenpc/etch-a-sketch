1b69e7b6b3179abf4958207dc46da4d2c8035ee4
dazenpc <121750352+dazenpc@users.noreply.github.com>
2025-01-19 14:36:59 +0530

    fix: refactor and remove old approach

--- scripts.js
+++ scripts.js
@@ -1,4 +1,7 @@
 const container = document.querySelector('.container')
+function colorThatOne(event) {
+    event.target.style["background-color"] = "blue"
+}
 
 for(let k = 0; k < 16; k++){
     const rowDiv = document.createElement('div')
@@ -6,18 +9,9 @@ for(let k = 0; k < 16; k++){
     for(let i = 0; i < 16; i++){
         const div = document.createElement('div')
         div.setAttribute("style", "height: 20px; width: 20px; border: 2px solid black;")
+        div.addEventListener("mouseover",colorThatOne)
         div.classList.add('el')
         rowDiv.appendChild(div)
     }
     container.appendChild(rowDiv)
-}
-
-function colorThatOne(event) {
-    event.target.style["background-color"] = "blue"
-}
-
-const el = document.querySelectorAll('.el')
-
-for(let element of el){
-    element.addEventListener("mouseover",colorThatOne)
 }
