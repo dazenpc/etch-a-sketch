08c151197dcb64c9917fe962a2404fb162e2c47c
dazenpc <121750352+dazenpc@users.noreply.github.com>
2025-01-19 14:34:37 +0530

    feat: make coloring logic

--- scripts.js
+++ scripts.js
@@ -5,10 +5,19 @@ for(let k = 0; k < 16; k++){
     rowDiv.setAttribute("style", "display: flex")
     for(let i = 0; i < 16; i++){
         const div = document.createElement('div')
-        div.setAttribute("style", "height: 20px; width: 20px; border: 2px solid black")
+        div.setAttribute("style", "height: 20px; width: 20px; border: 2px solid black;")
+        div.classList.add('el')
         rowDiv.appendChild(div)
     }
     container.appendChild(rowDiv)
 }
 
-console.log("hello")
+function colorThatOne(event) {
+    event.target.style["background-color"] = "blue"
+}
+
+const el = document.querySelectorAll('.el')
+
+for(let element of el){
+    element.addEventListener("mouseover",colorThatOne)
+}
