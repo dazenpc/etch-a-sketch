eb7056d36ab386c346f3e9c1a69f2af4a97afb48
dazenpc <121750352+dazenpc@users.noreply.github.com>
2025-01-19 14:56:25 +0530

    feat: add resize button

--- scripts.js
+++ scripts.js
@@ -1,17 +1,34 @@
+let size = 16
 const container = document.querySelector('.container')
 function colorThatOne(event) {
-    event.target.style["background-color"] = "blue"
+    event.target.style["background-color"] = "grey"
 }
 
-for(let k = 0; k < 16; k++){
-    const rowDiv = document.createElement('div')
-    rowDiv.setAttribute("style", "display: flex")
-    for(let i = 0; i < 16; i++){
-        const div = document.createElement('div')
-        div.setAttribute("style", "height: 20px; width: 20px; border: 2px solid black;")
-        div.addEventListener("mouseover",colorThatOne)
-        div.classList.add('el')
-        rowDiv.appendChild(div)
-    }
-    container.appendChild(rowDiv)
+function gridLayout(size){
+    container.replaceChildren();
+    for(let k = 0; k < size; k++){
+        const rowDiv = document.createElement('div')
+        rowDiv.setAttribute("style", "display: flex")
+        for(let i = 0; i < size; i++){
+            const div = document.createElement('div')
+            div.setAttribute("style", "height: 20px; width: 20px; border: 2px solid black;")
+            div.addEventListener("mouseover",colorThatOne)
+            rowDiv.appendChild(div)
+        }
+        container.appendChild(rowDiv)
 }
+}
+
+function getSize(){
+    let newSize = -1
+    while(!(newSize > 0 && newSize <= 100)){
+        newSize = prompt("Resize the grid [Max = 100]", 16)
+    }
+    gridLayout(newSize)
+}
+
+const resizeButton = document.querySelector('.resize')
+resizeButton.addEventListener("click", getSize)
+
+
+gridLayout(size)
